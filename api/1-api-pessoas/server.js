ae19fbb44bb364d2c5e1fc7c01a6a22d48f5d8da
davy-alt <davy.nicacio@aluno.senai.br>
2025-04-12 09:12:54 -0300

    laco da a bunda

--- api/1-api-pessoas/server.js
+++ api/1-api-pessoas/server.js
@@ -8,6 +8,13 @@ const app = express()
 app.use(express.json())
 const PORT = 3333
 
+    //TODO-LIST
+    // [X] - Listar todas as pessoas cadastradas
+    // [X] - Cadastrar uma unica pessoa
+    // [X] - Buscar uma unica pessoa
+    // [X] - Atualizar uma unica pessoa
+    // [] - Deletar uma unica pessoa
+
 const pessoas = []
 
 
@@ -42,17 +49,64 @@ app.post('/pessoas', (request, response) => {
 
 //Buscar uma única pessoa
 app.get('/pessoas/:id', (request, response) => {
-    response.status(200).json({rota: 'GET /pessoas/:id'})
+    const { id } = request.params
+
+    /**
+     *  {id:19, nome: "davy", cargo:"web developer"}
+     *  {id:20, nome: "davy", cargo:"web developer"}
+     */
+
+    const encontrarPessoa = pessoas.findIndex((obj) => obj.id === id)
+    if(encontrarPessoa === -1){
+        response.status(404).json({menssagem: 'Pessoa não encontrada.'})
+        return
+    }
+    //mostrar a pessoa procurada
+    const pessoa = pessoas [encontrarPessoa]
+
+    response.status(200).json({rota: 'Pessoa encontrada', pessoa})
 })
 
 //Atualizar uma única pessoa
 app.put('/pessoas/:id', (request, response) => {
-    response.status(200).json({rota: 'PUT /pessoas/:id'})
+    const { id } = request.params
+    const {nome, cargo} = request.body
+
+    const encontrarPessoa = pessoas.findIndex((obj) => obj.id === id)
+    if(encontrarPessoa === -1){
+        response.status(404).json({menssagem: 'Pessoa não encontrada.'})
+        return
+    }
+
+    if(!cargo || !nome){
+        response.status(400).json({messagem: 'Nome e cargo é obrigatorio'})
+        return
+    }
+
+    const pessoaAtualizada = {
+        id,
+        nome,
+        cargo
+    }
+
+    pessoas[encontrarPessoa] = pessoaAtualizada
+
+    response.status(200).json({rota: 'Pessoa Atualizada', pessoaAtualizada})
+    
 })
 
 //deletar uma única pessoa
 app.delete('/pessoas/:id', (request, response) => {
-    response.status(200).json({rota: 'DELETE /pessoas/:id'})
+    const { id } = request.params
+
+    
+    const encontrarPessoa = pessoas.findIndex((obj) => obj.id === id)
+    if(encontrarPessoa === -1){
+        response.status(404).json({menssagem: 'Pessoa não encontrada.'})
+        return
+    }
+    pessoas.splice(encontrarPessoa, 1)
+    response.status(200).json({menssagem:'Pessoa excluida'})
 })
 
 app.listen(PORT, () => {
